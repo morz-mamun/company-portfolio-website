a66f79877aac2974d79bc33b602d8af2b66b2628
morshed <tgc.morshed.alam@gmail.com>
2025-08-28 14:40:08 +0600

    fix: footer route aligment

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -81,7 +81,7 @@ export default function Footer() {
               {/* quick links */}
               <div className="flex flex-col space-y-3 lg:mx-auto">
                 <h4 className="text-lg font-semibold">Quick Links</h4>
-                <div className="space-y-2 text-sm">
+                <div className="flex flex-col items-start space-y-2 text-sm">
                   <Popover open={isOpen} onOpenChange={setIsOpen}>
                     <PopoverTrigger asChild>
                       <button className="cursor-pointer text-base hover:underline">
@@ -114,13 +114,13 @@ export default function Footer() {
                   </Popover>
                   <Link
                     href="/who-we-are"
-                    className="text-brand dark:text-primary hover:urderline text-xs transition-colors md:text-base"
+                    className="text-brand dark:text-primary hover:urderline text-xs transition-colors hover:underline md:text-base"
                   >
                     Who we are
                   </Link>
                   <Link
                     href="/contact-us"
-                    className="text-brand dark:text-primary hover:urderline text-xs transition-colors md:text-base"
+                    className="text-brand dark:text-primary hover:urderline text-xs transition-colors hover:underline md:text-base"
                   >
                     Contact
                   </Link>
