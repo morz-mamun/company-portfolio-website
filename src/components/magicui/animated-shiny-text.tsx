9ca5ccb94628c5c777cc66af80a8bce55b527d35
morshed <tgc.morshed.alam@gmail.com>
2025-09-08 15:03:01 +0600

    style: improve banner title button animation

--- src/components/magicui/animated-shiny-text.tsx
+++ src/components/magicui/animated-shiny-text.tsx
@@ -27,7 +27,7 @@ export const AnimatedShinyText: FC<AnimatedShinyTextProps> = ({
         'animate-shiny-text rounded-full [background-size:var(--shiny-width)_100%] [background-position:0_0] bg-no-repeat [transition:background-position_1s_cubic-bezier(.6,.6,0,1)_infinite]',
 
         // Shine gradient
-        'bg-gradient-to-r from-transparent via-[#ced5f1] via-50% to-transparent dark:via-black',
+        'bg-gradient-to-r from-transparent via-[#ced5f1] via-50% to-transparent dark:via-[#323a60] dark:via-50% dark:to-transparent',
 
         className,
       )}
