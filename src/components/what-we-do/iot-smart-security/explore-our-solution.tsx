825aee7779750e3e5e32e59efedf5d44b80beaec
morshed <tgc.morshed.alam@gmail.com>
2025-09-17 14:06:12 +0600

    docs: update content in iot smart security page

--- src/components/what-we-do/iot-smart-security/explore-our-solution.tsx
+++ src/components/what-we-do/iot-smart-security/explore-our-solution.tsx
@@ -1,10 +1,12 @@
 import SectionHeading from '@/components/shared/section-heading';
 import Service from '../shared/service/service';
 import {
-  cybersecurityForWebsitesAndIoTDevicesData,
-  industrialAndOfficeAutomationData,
-  iotAccessControlAndRealTimeMonitoringData,
-  smartAlarmsSensorsAndSurveillanceData,
+  accessControlAndBiometricSecurityData,
+  cloudBasedSecurityMonitoringData,
+  iotDeviceIntegrationAndAutomationData,
+  riskPreventionAndCybersecurityData,
+  servicesSectionTitleData,
+  smartSurveillanceSystemsData,
 } from '@/constants/what-we-do-data/iot-smart-security-data';
 import BorderXPattern from '@/components/shared/border-x-pattern';
 
@@ -12,17 +14,14 @@ export default function ExploreOurSolution() {
   return (
     <section className="pt-14">
       {/* section heading */}
-      <SectionHeading title="Explore Our Solutions" />
+      <SectionHeading title={servicesSectionTitleData} />
       <div className="relative border-y bg-[#FEFEFE] px-4 md:px-6 dark:bg-[#030712]">
         <div className="relative space-y-14 border-x py-14 md:space-y-32">
-          {/* Ai Chatbot and Voice Assistant */}
-          <Service sectionData={iotAccessControlAndRealTimeMonitoringData} />
-          {/* Workflow Automation */}
-          <Service sectionData={smartAlarmsSensorsAndSurveillanceData} />
-          {/* Multi-Agent Orchestration */}
-          <Service sectionData={cybersecurityForWebsitesAndIoTDevicesData} />
-          {/* Facility and Security Automation */}
-          <Service sectionData={industrialAndOfficeAutomationData} />
+          <Service sectionData={smartSurveillanceSystemsData} />
+          <Service sectionData={iotDeviceIntegrationAndAutomationData} />
+          <Service sectionData={accessControlAndBiometricSecurityData} />
+          <Service sectionData={cloudBasedSecurityMonitoringData} />
+          <Service sectionData={riskPreventionAndCybersecurityData} />
 
           {/* border-x pattern design */}
           <BorderXPattern className="absolute top-0 -left-4 w-4 md:-left-44 md:w-44" />
