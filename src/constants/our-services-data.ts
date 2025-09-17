55bbd33572cd9a5b9da15b90169d1fb7ff8d5bdb
morshed <tgc.morshed.alam@gmail.com>
2025-09-17 15:54:46 +0600

    fix: fixed explore our service data

--- src/constants/our-services-data.ts
+++ src/constants/our-services-data.ts
@@ -1,16 +1,16 @@
 export const servicesData = [
   {
-    title: 'AI Automation & Multi-Agent Systems',
+    title: 'Automation & AI Multi-Agent Systems',
     description:
-      'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution.',
+      'Revolutionize operations using intelligent AI agents and automation workflows that can reduce manual tasks, optimize productivity, and improve response time.',
     image: 'https://pub-3c3fbc3b66a54a4ab707f64d0f564e7d.r2.dev/service07.jpg',
-    link: '/ai-automation',
+    link: '/automation-ai-multi-agent-systems',
     className: 'lg:col-span-2 md:col-span-2',
   },
   {
     title: 'Web & Software Development',
     description:
-      'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution.',
+      'Reimagine digital experiences with fast, secure, and scalable websites, apps, and software that boost conversions, enhance performance, and grow with your business.',
     image: 'https://pub-3c3fbc3b66a54a4ab707f64d0f564e7d.r2.dev/service06.jpg',
     link: '/web-software-development',
     className: 'lg:col-span-2 md:col-span-2',
@@ -18,49 +18,49 @@ export const servicesData = [
   {
     title: 'Digital Marketing & SEO',
     description:
-      'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution.',
+      'Boost online visibility, drive traffic, and convert leads with AI-powered Digital Marketing & SEO strategies designed to scale businesses globally. Our services combine search optimization, paid advertising, content marketing, and automated funnels to maximize ROI and build measurable growth.',
     image: 'https://pub-3c3fbc3b66a54a4ab707f64d0f564e7d.r2.dev/service03.jpg',
     link: '/digital-marketing-seo',
     className: 'lg:col-span-2 lg:row-span-2 md:col-span-4',
   },
   {
-    title: 'SaaS & Affiliate Tools',
+    title: 'SaaS Products & Automation Tools',
     description:
-      'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution.',
+      'Develop scalable SaaS platforms and AI-powered automation tools that save time, reduce costs, and maximize business growth. Our solutions help brands streamline operations, enhance user experiences, and achieve measurable results across digital channels.',
     image: 'https://pub-3c3fbc3b66a54a4ab707f64d0f564e7d.r2.dev/service04.jpg',
-    link: '/saas-affiliated-tools',
+    link: '/saas-automation-tools',
     className: 'lg:col-span-4 md:col-span-4',
   },
   {
-    title: 'E-commerce & Affiliate Services',
+    title: 'E-commerce Management Services',
     description:
-      'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution.',
+      'Launch, optimize, and automate online stores and marketplace operations with AI-powered solutions that boost visibility, increase conversions, and streamline day-to-day management.',
     image: 'https://pub-3c3fbc3b66a54a4ab707f64d0f564e7d.r2.dev/service02.png',
     link: '/e-commerce-affiliate-services',
     className: 'lg:col-span-3 md:col-span-2',
   },
   {
-    title: 'IoT & Smart Security Solutions',
+    title: 'AI BPO Solutions',
     description:
-      'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution.',
+      'Transform outsourcing with AI-powered solutions that streamline operations, reduce costs, and improve accuracy across customer support, back-office, and sales processes.',
     image: 'https://pub-3c3fbc3b66a54a4ab707f64d0f564e7d.r2.dev/service01.png',
-    link: '/iot-smart-security-solutions',
+    link: '/ai-bpo-solutions',
     className: 'lg:col-span-3 md:col-span-2',
   },
   {
-    title: 'E-commerce & Affiliate Services',
+    title: 'IoT & Smart Security Solutions',
     description:
-      'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution.',
+      'Connect devices, protect assets, and automate monitoring with intelligent IoT integration and AI-powered security systems. Our solutions help businesses reduce risks, improve compliance, and enhance operational efficiency.',
     image: 'https://pub-3c3fbc3b66a54a4ab707f64d0f564e7d.r2.dev/service02.png',
-    link: '/e-commerce-affiliate-services',
+    link: '/iot-smart-security-solutions',
     className: 'lg:col-span-3 md:col-span-2',
   },
   {
-    title: 'IoT & Smart Security Solutions',
+    title: 'Emerging AI Services (Future-Ready)',
     description:
-      'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution.',
+      'Anticipate the future and gain a competitive edge with AI innovations that drive growth, efficiency, and personalization across industries.',
     image: 'https://pub-3c3fbc3b66a54a4ab707f64d0f564e7d.r2.dev/service01.png',
-    link: '/iot-smart-security-solutions',
+    link: '/emerging-ai-services',
     className: 'lg:col-span-3 md:col-span-2',
   },
 ];
